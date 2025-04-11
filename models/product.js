e8c4b02feaeabc8b7ef79f74af4b9b219b7b145b
Nihal <nihalranathunge@gmail.com>
2025-04-11 06:24:32 +0530

    Inirializing the project

---
+++ models/product.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    productId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    image: [
+        {type: String},
+    ],
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    isAvailable: {
+        type: Boolean,
+        required: true    
+    }   
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
